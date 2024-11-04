8e3cef3baba7a2120187cd55aecbecbb046f097f
DESKTOP-TM4I4ST\Duarte <duartemv00@gmail.com>
2024-11-04 23:03:35 +0100

    Battery functionality P1

--- Source/DMV_FlashLightComp/Private/FlashlightComponent.cpp
+++ Source/DMV_FlashLightComp/Private/FlashlightComponent.cpp
@@ -7,12 +7,17 @@
 #include "GameFramework/Character.h"
 #include "GameFramework/SpringArmComponent.h"
 
-
+/**
+ * Constructor
+ */
 UFlashlightComponent::UFlashlightComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 }
 
+/**
+ * Called when the game starts
+ */
 void UFlashlightComponent::BeginPlay()
 {
 	Super::BeginPlay();
@@ -20,12 +25,20 @@ void UFlashlightComponent::BeginPlay()
 	ConfigureFlashlight();
 }
 
+/**
+ * Construct the flashlight components
+ * @return Whether the flashlight components were constructed successfully
+ */
 bool UFlashlightComponent::ConstructFlashlight()
 {
 	if(AddSpringArm()) if(AddSpotLight()) return true;
 	return false;
 }
 
+/**
+ * Add a SpringArm component to the owner
+ * @return Whether the SpringArm component was added successfully
+ */
 bool UFlashlightComponent::AddSpringArm()
 {
 	UActorComponent* SpringArmCreation = GetOwner()->AddComponentByClass(
@@ -36,9 +49,9 @@ bool UFlashlightComponent::AddSpringArm()
 	SpringArm = Cast<USpringArmComponent>(SpringArmCreation);
 
 	// Attach SpringArm to Owner
-	SpringArm->SetWorldLocation(GetOwner()->GetActorLocation());
 	SpringArm->AttachToComponent(
 		Cast<USceneComponent>(GetOwner()->GetRootComponent()), FAttachmentTransformRules::KeepRelativeTransform, NAME_None);
+	SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
 
 	SpringArm->TargetArmLength = 0.0f;
 	SpringArm->bUsePawnControlRotation = true;
@@ -47,6 +60,10 @@ bool UFlashlightComponent::AddSpringArm()
 	return true;
 }
 
+/**
+ * Add a SpotLight component to the owner
+ * @return Whether the SpotLight component was added successfully
+ */
 bool UFlashlightComponent::AddSpotLight()
 {
 	UActorComponent* SpotLightCreation = GetOwner()->AddComponentByClass(
@@ -57,48 +74,131 @@ bool UFlashlightComponent::AddSpotLight()
 	SpotLight = Cast<USpotLightComponent>(SpotLightCreation);
 	
 	// Attach SpotLight to SpringArm
-	SpotLight->SetWorldLocation(GetOwner()->GetActorLocation());
 	SpotLight->AttachToComponent(SpringArm, FAttachmentTransformRules::KeepRelativeTransform, NAME_None);
+	SpotLight->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
 	
 	return true;
 }
 
+/**
+ * Set the flashlight properties
+ * @param bInDebug Activate debug messages and visualization
+ * @param InMaterial Material to mask the flashlight
+ * @param bInStartOn Whether the flashlight should start on 
+ * @param bInBlock Whether the flashlight should be blocked, so you can't turn it on or off 
+ * @param InLagSpeed The speed of the flashlight rotation to catch up with the camera rotation
+ * @param InLocation The relative location of the flashlight to the owner
+ * @param InIntensity The intensity of the flashlight light
+ * @param bInUseBattery Whether the flashlight should use battery
+ * @param InBatteryLife The total battery life of the flashlight
+ * @param InBatteryDrainRatePerSecond The rate at which the battery drains per second
+ * @param InBatteryRechargeRatePerSecond The rate at which the battery recharges per second
+ */
 void UFlashlightComponent::SetFlashlightProperties(
-	UMaterialInstance* InMaterial,
+		bool bInDebug,
+		UMaterialInstance* InMaterial,
 		bool bInStartOn,
 		bool bInBlock,
 		float InLagSpeed,
 		FVector InLocation,
-		float InIntensity)
+		float InIntensity,
+		bool bInUseBattery,
+		float InBatteryLife,
+		float InBatteryDrainRatePerSecond,
+		float InBatteryRechargeRatePerSecond)
 {
+	bDebug = bInDebug;
 	FlashlightLocation = InLocation;
 	FlashlightLagSpeed = InLagSpeed;
 	FlashlightIntensity = InIntensity;
 	FlashlightMaskMaterial = InMaterial;
 	bBlockFlashlight = bInBlock;
 	bFlashLightOn = bInStartOn;
+	bUseBattery = bInUseBattery;
+	BatteryLife = InBatteryLife;
+	BatteryDrainRatePerSecond = InBatteryDrainRatePerSecond;
+	BatteryRechargeRatePerSecond = InBatteryRechargeRatePerSecond;
+
+	ConfigureFlashlight();
 }
 
+/**
+ * Configure the flashlight with the setted properties
+ */
 void UFlashlightComponent::ConfigureFlashlight()
 {
-	SpringArm->SetWorldLocation(FlashlightLocation);
+	SpringArm->SetRelativeLocation(FlashlightLocation);
 	SpringArm->CameraRotationLagSpeed = FlashlightLagSpeed;
 	SpotLight->SetIntensity(FlashlightIntensity);
 	if(FlashlightMaskMaterial) SpotLight->SetLightFunctionMaterial(FlashlightMaskMaterial);
-	if(bFlashLightOn) SpringArm->SetVisibility(true, true);
-	else SpringArm->SetVisibility(false, true);
+	if(bFlashLightOn) FlashlightOn();
+	else FlashlightOff();
 }
 
+/**
+ * Toggle the flashlight on and off
+ */
 void UFlashlightComponent::ToggleFlashlight()
+{
+	if (bFlashLightOn) {
+		FlashlightOff();
+	} else {
+		FlashlightOn();
+	}
+}
+
+/**
+ * Turn on the flashlight
+ */
+void UFlashlightComponent::FlashlightOn()
 {
 	if(!bBlockFlashlight) // Check if flashlight is blocked
 	{
-		if (bFlashLightOn) {
-			SpringArm->SetVisibility(false, true);
-			bFlashLightOn = false;
-		} else {
-			SpringArm->SetVisibility(true, true);
-			bFlashLightOn = true;
-		}
+		SpringArm->SetVisibility(true, true);
+		bFlashLightOn = true;
+		if(bUseBattery) Start_ReduceBatteryTimer();
 	}
+}
+
+/**
+ * Turn off the flashlight
+ */
+void UFlashlightComponent::FlashlightOff()
+{
+	if(!bBlockFlashlight) // Check if flashlight is blocked
+	{
+		SpringArm->SetVisibility(false, true);
+		bFlashLightOn = false;
+		if(bUseBattery) Stop_ReduceBatteryTimer();
+	}
+}
+
+/**
+ * Call the timer function to reduce the battery life
+ */
+void UFlashlightComponent::Start_ReduceBatteryTimer()
+{
+	GetWorld()->GetTimerManager().SetTimer(BatteryTimerHandle, this,
+		&UFlashlightComponent::ReduceBatteryTimer, 1.f, true, 0.f);
+}
+
+/**
+ * Stop the timer function to reduce the battery life
+ */
+void UFlashlightComponent::Stop_ReduceBatteryTimer()
+{
+	GetWorld()->GetTimerManager().ClearTimer(BatteryTimerHandle);
+}
+
+/**
+ * Timer function that reduces the battery life
+ */
+void UFlashlightComponent::ReduceBatteryTimer()
+{
+	if(BatteryLife > 0.0f)
+	{
+		BatteryLife -= BatteryDrainRatePerSecond;
+		if (bDebug) GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, FString::Printf(TEXT("Battery: %f"), BatteryLife));
+	}
+	else FlashlightOff();
 }
--- Source/DMV_FlashLightComp/Public/FlashlightComponent.h
+++ Source/DMV_FlashLightComp/Public/FlashlightComponent.h
@@ -19,6 +19,9 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	bool bDebug = false;
 	
 	// BEGIN - Flashlight components construction
 	UFUNCTION(BlueprintCallable)
@@ -30,39 +33,74 @@ protected:
 	// BEGIN - Flashlight configuration
 	UFUNCTION(BlueprintCallable)
 	void SetFlashlightProperties(
+		bool bInDebug,
 		UMaterialInstance* InMaterial,
 		bool bInStartOn,
 		bool bInBlock,
 		float InLagSpeed,
 		FVector InLocation,
-		float InIntensity);
+		float InIntensity,
+		bool bInUseBattery,
+		float InBatteryLife,
+		float InBatteryDrainRatePerSecond,
+		float InBatteryRechargeRatePerSecond);
+
 	void ConfigureFlashlight();
 	// END - Flashlight configuration
 	
 	// BEGIN - Flashlight functionality
 	UFUNCTION(BlueprintCallable)
 	void ToggleFlashlight();
+	UFUNCTION(BlueprintCallable)
+	void FlashlightOn();
+	UFUNCTION(BlueprintCallable)
+	void FlashlightOff();
 	// END - Flashlight functionality
 
 private:
 	// Flashlight properties
+
 	UPROPERTY(EditAnywhere, Category = "Flashlight")
 	UMaterialInstance* FlashlightMaskMaterial;
+
 	UPROPERTY(EditAnywhere, Category = "Flashlight")
 	float FlashlightLagSpeed = 10.0f;
+
 	UPROPERTY(EditAnywhere, Category = "Flashlight")
 	float FlashlightIntensity = 100000.0f;
+
 	UPROPERTY(EditAnywhere, Category = "Flashlight")
 	FVector FlashlightLocation = FVector(0.0f, 20.0f, 40.0f);
+
 	UPROPERTY(EditAnywhere, Category = "Flashlight")
 	bool bBlockFlashlight = false;
 
+	// Batery properties
+	UPROPERTY(EditAnywhere, Category = "Flashlight|Battery")
+	bool bUseBattery = false;
+	
+	UPROPERTY(EditAnywhere, Category = "Flashlight|Battery")
+	float BatteryLife = 100.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Flashlight|Battery")
+	float BatteryDrainRatePerSecond = 0.01f;
+
+	UPROPERTY(EditAnywhere, Category = "Flashlight|Battery")
+	float BatteryRechargeRatePerSecond = 0.01f;
+
+	FTimerHandle BatteryTimerHandle;
+	void Start_ReduceBatteryTimer();
+	void Stop_ReduceBatteryTimer();
+	void ReduceBatteryTimer();
+
 	// Components
-	UPROPERTY(EditAnywhere, Category = "Flashlight")
+	UPROPERTY()
 	USpringArmComponent* SpringArm;
-	UPROPERTY(EditAnywhere, Category = "Flashlight")
+
+	UPROPERTY()
 	USpotLightComponent* SpotLight;
 
 	// Internal flashlight state
+	UPROPERTY()
 	bool bFlashLightOn = true;
 };
